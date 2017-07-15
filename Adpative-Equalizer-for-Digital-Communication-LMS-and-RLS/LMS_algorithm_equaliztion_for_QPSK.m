1a6d426c96204fa4efa6b4c98e6275feae3be740
Andrew <t2tt2tt51.eed05g@g2.nctu.edu.tw>
2017-07-15 21:19:49 +0800

    Update LMS_algorithm_equaliztion_for_QPSK.m

--- Adpative-Equalizer-for-Digital-Communication-LMS-and-RLS/LMS_algorithm_equaliztion_for_QPSK.m
+++ Adpative-Equalizer-for-Digital-Communication-LMS-and-RLS/LMS_algorithm_equaliztion_for_QPSK.m
@@ -1,11 +1,11 @@
-%%% Adaptive Equalization for Digital Communications - Least Mean Square algorith %%%
-%%% Two-ray multipath channel %%%
+% Adaptive Equalization for Digital Communications - Least Mean Square algorith 
+% Two-ray multipath channel 
 
-%%% Reset MATLAB %%%
+% Reset MATLAB 
 clear;
 clc;
 
-%%% Parameter setting %%%
+% Parameter setting 
 tx_leng = 128;        % Transmission length
 M = 5;                % Half length of response -M ~ M ( length = 2*M+1 )
 ch_leng = M*2 + 1;    % Channel response length 
@@ -14,7 +14,7 @@ p2_phase = (7*pi)/8;  % Paht 2 channel response phase
 beta = 0.35;          % For TDMA digital cellular standard
 w_length = 12;        % Filter length
 
-%%% Initialization of varible %%%
+% Initialization of varible 
 tx = zeros(1,tx_leng);            % Transmitted symbols
 ch = zeros(1,ch_leng);            % Channel response
 tx_hat = zeros(1,tx_leng);        % Estimation of transmitted symbols
@@ -22,21 +22,21 @@ error = 0;                        % LMS algorithm error
 w = zeros(w_length,1);            % Filter
 miu = 10^(-1);                       % LMS algorithm learning rate
 
-%%% Transmitted symbols %%%
+% Transmitted symbols 
 for t = 1:tx_leng                 
 	tx(1,t) = sgn(-2*rand+1) + j*sgn(-2*rand+1);  % Random generationn
 end
 
-%%$ Channel response %%%
+% Channel response 
 for t = 1:ch_leng                 % Channel is assumed to have raised cosine spectrum
     ch(1,t) = p2_abs*exp(j*p2_phase)*(sin(pi*(t-(M+1)-0.5))/(pi*(t-(M+1)-0.5)))*(cos(beta*pi*(t-(M+1)-0.5))/(1-4*(beta^2)*(t-(M+1)-0.5)^2));
 end
 ch(1,M+1) = ch(1,M+1) + 1;        % Impulse response 
 
-%%% Received signals %%%
+% Received signals 
 rx = [conv(tx,ch),0];             % Received symbols ( Time domain )
 
-%%% Least mean square algorithm %%%
+% Least mean square algorithm 
 for t = 1:tx_leng
 	rx_tmp = zeros(w_length,1);
 	for k = 1:w_length
@@ -47,7 +47,7 @@ for t = 1:tx_leng
 	w = w + miu*conj(error)*rx_tmp;  % Update the filter weigths
 end
 
-%%% Plot diagram %%%
+% Plot diagram 
 figure(1);
 
 scatter(real(tx),imag(tx),200,'g','filled'); % Transmitted symbols
