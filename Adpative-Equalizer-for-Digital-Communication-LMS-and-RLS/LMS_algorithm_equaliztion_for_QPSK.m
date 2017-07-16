235d3225d9fca68348462c0d67c04957752b8b0a
Andrew <t2tt2tt51.eed05g@g2.nctu.edu.tw>
2017-07-16 23:16:19 +0800

    Update LMS_algorithm_equaliztion_for_QPSK.m

--- Adpative-Equalizer-for-Digital-Communication-LMS-and-RLS/LMS_algorithm_equaliztion_for_QPSK.m
+++ Adpative-Equalizer-for-Digital-Communication-LMS-and-RLS/LMS_algorithm_equaliztion_for_QPSK.m
@@ -1,69 +1,68 @@
-% Adaptive Equalization for Digital Communications - Least Mean Square algorith 
+% Adaptive Equalization for Digital Communications - Least Mean Square filter
 % Two-ray multipath channel 
 
-% Reset MATLAB 
+% Reset MATLAB variables 
 clear;
 clc;
 
 % Parameter setting 
-tx_leng = 128;        % Transmission length
-M = 5;                % Half length of response -M ~ M ( length = 2*M+1 )
-ch_leng = M*2 + 1;    % Channel response length 
-p2_abs = 0.5;         % Path 2 channel response amplitude
-p2_phase = (7*pi)/8;  % Paht 2 channel response phase
-beta = 0.35;          % For TDMA digital cellular standard
-w_length = 12;        % Filter length
+d_leng = 128;            % Length of sequence of transmitted symbols
+M1 = 5;               
+M2 = 5;               
+q_leng = M1 + M2 + 1;    % Length of the multipath distorted pulse symbol waveform
+g2_abs = 0.5;            % Path 2 channel response amplitude
+g2_phase = (7*pi)/8;     % Paht 2 channel response phase
+beta = 0.35;             % For TDMA digital cellular standard
+w_length = 12;           % Length of filter taps 
 
 % Initialization of varible 
-tx = zeros(1,tx_leng);            % Transmitted symbols
-ch = zeros(1,ch_leng);            % Channel response
-tx_hat = zeros(1,tx_leng);        % Estimation of transmitted symbols
-error = 0;                        % LMS algorithm error
-w = zeros(w_length,1);            % Filter
+d = zeros(1,d_leng);              % Sequence of transmitted symbols
+q = zeros(1,q_leng);              % Multipath distorted pulse symbol waveform
+d_hat = zeros(1,d_leng);          % Estimation of transmitted symbols
+e = 0;                            % LMS filter error
+w = zeros(w_length,1);            % Filter taps
 miu = 10^(-1);                    % LMS algorithm learning rate
 
-% Transmitted symbols 
-for t = 1:tx_leng                 
-	tx(1,t) = sgn(-2*rand+1) + j*sgn(-2*rand+1);  % Random generationn
+% Sequence of transmitted symbols
+for n = 1:d_leng                 
+	d(1,n) = sgn(-2*rand+1) + j*sgn(-2*rand+1);  % Random generationn
 end
 
-% Channel response 
-for t = 1:ch_leng                 % Channel is assumed to have raised cosine spectrum
-    ch(1,t) = p2_abs*exp(j*p2_phase)*(sin(pi*(t-(M+1)-0.5))/(pi*(t-(M+1)-0.5)))*(cos(beta*pi*(t-(M+1)-0.5))/(1-4*(beta^2)*(t-(M+1)-0.5)^2));
+% Multipath distorted pulse symbol waveform 
+for n = 1:q_leng                  % Waveform  is assumed to have raised cosine spectrum
+    q(1,n) = g2_abs*exp(j*g2_phase)*(sin(pi*(n-(M1+1)-0.5))/(pi*(n-(M1+1)-0.5)))*(cos(beta*pi*(n-(M1+1)-0.5))/(1-4*(beta^2)*(n-(M1+1)-0.5)^2));
 end
-ch(1,M+1) = ch(1,M+1) + 1;        % Impulse response 
+q(1,M1+1) = q(1,M1+1) + 1;        % Impulse response 
 
-% Received signals 
-rx = [conv(tx,ch),0];             % Received symbols ( Time domain )
+% Sequence of received symbols
+u = [conv(d,q),0];                % Sequence of received symbols
 
 % Least mean square algorithm 
-for t = 1:tx_leng
-	rx_tmp = zeros(w_length,1);
+for n = 1:d_leng
+	u_tmp = zeros(w_length,1);
 	for k = 1:w_length
-		rx_tmp(k) = rx(w_length-k+t);
+		u_tmp(k) = u(w_length-k+n);
     end
-	tx_hat(1,t) = w'*rx_tmp;
-	error = tx(1,t) - tx_hat(1,t);  
-	w = w + miu*conj(error)*rx_tmp;  % Update the filter weigths
+	d_hat(1,n) = w'*u_tmp;
+	e = d(1,n) - d_hat(1,n);  
+	w = w + miu*conj(e)*u_tmp;  % Update the filter weigths
 end
 
 % Plot diagram 
 figure(1);
 
-scatter(real(tx),imag(tx),200,'g','filled'); % Transmitted symbols
+scatter(real(d),imag(d),200,'g','filled'); % Transmitted symbols
 hold on;
 
-scatter(real(rx),imag(rx),20,'r');           % Received symbols
+scatter(real(u),imag(u),20,'r');           % Received symbols
 hold on;
 
-scatter(real(tx_hat),imag(tx_hat),20,'b');   % LMS estimation
+scatter(real(d_hat),imag(d_hat),20,'b');   % LMS estimation
 hold on;
 
 axis([-2,2,-2,2]);                        
-legend('Transmitted symbols','Received symbols','LMS estimation');
-title('LMS algorithm equalization for QPSK');
+legend('Transmitted symbols','Received symbols','LMS estimation')
+title('LMS filter equalization for QPSK');
 xlabel('Real');
 ylabel('Imaginary');
-
-
 
